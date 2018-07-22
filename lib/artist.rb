9e876f55b46f2a30ef76264e4da268274471f4d2
Sushi Chanrai <sushi@pathworks.co>
2018-07-22 14:25:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,10 +16,10 @@ class Artist
     #@@artists.detect{|a| a.name == name}
   #end
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+  #def initialize
+    #@@artists << self
+    #@songs = []
+  #end
 
   def self.all
     @@artists
